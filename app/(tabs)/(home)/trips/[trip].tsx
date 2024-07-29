b0252922eb5698c19e681504855928c27f062105
unnipv <unnimonpv@gmail.com>
2024-07-30 02:17:36 +0530

    we can edit names now

--- app/(tabs)/(home)/trips/[trip].tsx
+++ app/(tabs)/(home)/trips/[trip].tsx
@@ -1,13 +1,21 @@
-import { Link, Stack, useLocalSearchParams } from 'expo-router';
+import { Link, Stack, useLocalSearchParams, useRouter } from 'expo-router';
 import { View, Text, StyleSheet } from 'react-native';
 
 export default function TripScreen() {
+  const router = useRouter();
+  // const { trip } = useLocalSearchParams();
   const params = useLocalSearchParams();
   
   return (
     <View style={styles.container} >
       <Stack.Screen options={{ title: params.trip}} />
-      <Text>Details of {trip}</Text>
+      <Text>Details of {params.trip}</Text>
+      <Text
+        onPress={() => {
+          router.setParams({ trip: 'kasol' })
+        }}>
+          change trip to kasol
+        </Text>
       <Link href="/">go back home.</Link>
     </View>
   );
